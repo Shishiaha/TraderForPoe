53b467bd4ea7c5d2bcaa45020cbc4174c3d7fa56
Ark4nsas <finalman3@hotmail.com>
2025-02-03 11:40:49 +0100

    Updater updated

--- TraderForPoe/Properties/AssemblyInfo.cs
+++ TraderForPoe/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("TraderForPoe")]
+[assembly: AssemblyTitle("TraderForPoe2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.4.0.2")]
+[assembly: AssemblyFileVersion("2.4.0.2")]
